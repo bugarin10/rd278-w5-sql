75324f2c9af3193aa41c312b88536f276f8648f6
Rafael Davila Bugarin <rafaeldavila.b@gmail.com>
2023-09-26 18:30:08 +0000

    linting

--- pythonproject/src/building_database.py
+++ pythonproject/src/building_database.py
@@ -1,22 +1,22 @@
+"""
+This module creates a SQLite database from a CSV file.
+"""
+
 import sqlite3
 import csv
-import os
-
-# print(os.getcwd())
 
 # Step 1: Establish a connection to the SQLite database
 conn = sqlite3.connect("pythonproject/src/data/happiness_database.db")
-
 
 # Step 2: Create a cursor object
 cursor = conn.cursor()
 
 # Step 3: Drop the table if it exists
-drop_table_sql = "DROP TABLE IF EXISTS happiness_table"
-cursor.execute(drop_table_sql)
+DROP_TABLE_SQL = "DROP TABLE IF EXISTS happiness_table"
+cursor.execute(DROP_TABLE_SQL)
 
 # Step 4: Create a table with the desired schema
-create_table_sql = """
+CREATE_TABLE_SQL = """
 CREATE TABLE happiness_table (
     country TEXT PRIMARY KEY,
     log_gdp_pc FLOAT,
@@ -25,21 +25,18 @@ CREATE TABLE happiness_table (
     freedom FLOAT,
     generosity FLOAT,
     corruption FLOAT
-
 )
 """
-cursor.execute(create_table_sql)
+cursor.execute(CREATE_TABLE_SQL)
 
 # Step 5: Open the CSV file and populate the SQLite table
+CSV_FILE_PATH = "pythonproject/src/data/happiness_data.csv"
 
-csv_file_path = "pythonproject/src/data/happiness_data.csv"
-
-with open(csv_file_path, "r", newline="", encoding="utf-8") as csv_file:
+with open(CSV_FILE_PATH, "r", newline="", encoding="utf-8") as csv_file:
     csv_reader = csv.reader(csv_file)
     next(csv_reader)  # Skip the header row if it exists
 
     for row in csv_reader:
-        # Assuming your CSV has columns: id, name, age, city
         cursor.execute(
             "INSERT INTO happiness_table(country, log_gdp_pc, social_support, life_exp, freedom, generosity, corruption) VALUES (?, ?, ?, ?, ?, ?, ?)",
             row,
--- pythonproject/src/queries.py
+++ pythonproject/src/queries.py
@@ -1,23 +1,25 @@
+"""
+This module contains queries for the happiness database.
+"""
+
 import sqlite3
 
 # Step 1: Establish a connection to the SQLite database
-conn = sqlite3.connect("data/happiness_database.db")
-
-# print(f"connection: {conn}")
+conn = sqlite3.connect("pythonproject/src/data/happiness_database.db")
 
 # Step 2: Create a cursor object
 cursor = conn.cursor()
 
 # First query: Select all countries where social support is greater than the average social support
-query_1 = "SELECT country FROM happiness_table WHERE social_support > (SELECT AVG(social_support) FROM happiness_table)"
+QUERY_1 = "SELECT country FROM happiness_table WHERE social_support > (SELECT AVG(social_support) FROM happiness_table)"
 
-for i in cursor.execute(query_1):
+for i in cursor.execute(QUERY_1):
     print(i)
 
 # Second query:
-query_2 = "SELECT country FROM happiness_table WHERE country LIKE 'A%'"
+QUERY_2 = "SELECT country FROM happiness_table WHERE country LIKE 'A%'"
 
-for i in cursor.execute(query_2):
+for i in cursor.execute(QUERY_2):
     print(i)
 
 conn.close()
