56f99501acbdfde5bbb49e07ec01ee53b9ea70a0
Rafael Davila Bugarin <rafaeldavila.b@gmail.com>
2023-09-27 23:43:14 +0000

    including fire

--- pythonproject/src/queries.py
+++ pythonproject/src/queries.py
@@ -1,8 +1,9 @@
 """
 This module contains queries for the happiness database.
 """
-
+import fire
 import sqlite3
+import os
 
 # Step 1: Establish a connection to the SQLite database
 conn = sqlite3.connect("pythonproject/src/data/happiness_database.db")
@@ -11,16 +12,41 @@ conn = sqlite3.connect("pythonproject/src/data/happiness_database.db")
 cursor = conn.cursor()
 
 # First query: Select all countries where social support is greater than the average social support
-QUERY_1 = "SELECT country FROM happiness_table WHERE social_support > \
-(SELECT AVG(social_support) FROM happiness_table)"
+QUERY_1 = "SELECT * FROM happiness_table WHERE social_support > \
+(SELECT AVG(social_support) FROM happiness_table) LIMIT 3"
 
 for i in cursor.execute(QUERY_1):
     print(i)
+print("\n")
 
 # Second query:
 QUERY_2 = "SELECT country FROM happiness_table WHERE country LIKE 'A%'"
 
 for i in cursor.execute(QUERY_2):
     print(i)
-
+print("\n")
 conn.close()
+
+
+def main(query):
+    """
+    Main function for the queries module.
+    """
+    # Step 1: Establish a connection to the SQLite database
+    conn = sqlite3.connect("pythonproject/src/data/happiness_database.db")
+
+    # Step 2: Create a cursor object
+    cursor = conn.cursor()
+
+    # First query: Select all countries where social support is greater than the average social support
+    QUERY_1 = query
+
+    for i in cursor.execute(QUERY_1):
+        print(i)
+    print("\n")
+
+    conn.close()
+
+
+if __name__ == "__main__":
+    fire.Fire(main)
